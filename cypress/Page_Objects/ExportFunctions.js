da423b6e4cdc604af3a15400ccbb416911057525
m-2105 <muqaddasfarooq90@gmail.com>
2021-06-09 04:20:25 -0700

    Updated Functions

--- cypress/Page_Objects/ExportFunctions.js
+++ cypress/Page_Objects/ExportFunctions.js
@@ -1,93 +1,122 @@
 /// <reference types="cypress' / >
 import '../fixtures/inkforall_url.json'
-export function navigateURl(url){
-    cy.visit(url)
+
+//Function for Saving Local Storage [Will be corrected by tomorrow]
+export function NavigateForlocalStorage() {
+  cy.visit('https://ink-test.seo.app?code=4ds34s-231sed2-123sde-32s2332')
+  cy.saveLocalStorage()
 }
 
-export function navigate(n){
-  cy.fixture('inkforall_url').then(function (data) {
-  this.data = data;
-  navigateURl(this.data[n].testurl)
-})
+//Function for Navigating URl
+export function navigateURl(url) {
+  cy.visit(url)
 }
 
-export function validation(n){
-  cy.fixture('inkforall_url').then(function (data) {
-  this.data = data;
-  cy.url().should('contain',this.data[n].testurl)
-  })
-}
-export function navbar(){
- for(var i=1; i<=6; i++ ){
-  buttonClick('.nav-02__list--desktop > :nth-child('+ i +') > .button')
-  validation(i)
- navigate(0)
- validation(0)
-}
-}
-
- export function downloadApp(Path,here){
+//Function for Navigating URl though fixture file 
+export function navigate(n) {
   cy.fixture('inkforall_url').then(function (data) {
     this.data = data;
-  for(var i=1; i<=3; i++ ){
-   cy.get(Path + i +') > .button').should("have.attr", "href", this.data[i+7].testurl);
-  }
-  cy.get(here).should("have.attr", "href", this.data[30].testurl);
-})
+    navigateURl(this.data[n].testurl)
+  })
 }
 
- export function buttonClick(path){
-    cy.get(path,{timeout: 700000}).should('be.visible');
-    cy.get(path).click()
-     // { multiple: true } 
-   }
+//Function for Clicking a Button
+export function buttonClick(path) {
+  cy.get(path, { timeout: 700000 }).should('be.visible');
+  cy.get(path).click()
+}
 
-   export function footer(){
+//Function for Validating URl from fixture file 
+export function validation(n) {
+  cy.fixture('inkforall_url').then(function (data) {
+    this.data = data;
+    cy.url().should('contain', this.data[n].testurl)
+  })
+}
 
-    buttonClick('.footer-04__logo')  
-    validation(28) 
+//Function for Validating URl from fixture file With DOM VIEW
+export function domValidation(Path, n) {
+  cy.fixture('inkforall_url').then(function (data) {
+    this.data = data;
+    cy.get(Path).should("have.attr", "href", this.data[n].testurl);
+  })
+}
 
-    for(var i=2; i<=4; i++){
-      if (i==2 || i==4){
-        for(var j=1; j<=2; j++){
-         buttonClick(':nth-child('+i+') > .footer-04__list > :nth-child('+j+') > .footer-04__link')
+//Function for Validating all the NavBar items URL
+export function navbar() {
+  for (var i = 1; i <= 6; i++) {
+    buttonClick('.nav-02__list--desktop > :nth-child(' + i + ') > .button')
+    validation(i)
+    navigate(0)
+    validation(0)
+  }
+}
 
-         if(i==2)
-         validation(j+15)
-         if(i==4)
-         validation(j+21)
-
-         navigate(0)
-         validation(0)
-        }
-      }
-      else{
-       for(var j=1; j<=4; j++){
-        buttonClick(':nth-child('+i+') > .footer-04__list > :nth-child('+j+') > .footer-04__link')
-
-        if(i==3) if(i==4){
-          if(j == 1 || j == 2){
-           cy.get(':nth-child('+i+') > .footer-04__list > :nth-child('+j+') > .footer-04__link').should("have.attr", "href", this.data[j+17].testurl);
-          }
-          else
-          validation(j+17)
-         }
-         
-         if(i==5){
-          if(j == 1 || j == 4){
-           cy.get(':nth-child('+i+') > .footer-04__list > :nth-child('+j+') > .footer-04__link').should("have.attr", "href", this.data[j+23].testurl);
-          }
-          else
-          validation(j+23)
-         }
-         
- 
-         navigate(0)
-         validation(0)
-
-        }
-      }
-      }
-
-      //
+//Function for Validating all the Download App Button URL
+export function downloadApp(Path, here) {
+  cy.fixture('inkforall_url').then(function (data) {
+    this.data = data;
+    for (var i = 1; i <= 3; i++) {
+      domValidation(Path + i + ') > .button', i + 7)
     }
+    domValidation(here, 30)
+  })
+}
+
+//Function for Validating all the Items in footer Except Share Icons
+export function footer() {
+  buttonClick('.footer-04__logo')
+  validation(28)
+
+  for (var i = 2; i <= 4; i++) {
+    if (i == 2 || i == 4) {
+      for (var j = 1; j <= 2; j++) {
+        buttonClick(':nth-child(' + i + ') > .footer-04__list > :nth-child(' + j + ') > .footer-04__link')
+
+        if (i == 2)
+          validation(j + 15)
+        if (i == 4)
+          validation(j + 21)
+
+        navigate(0)
+        validation(0)
+      }
+    }
+    else {
+      for (var j = 1; j <= 4; j++) {
+        buttonClick(':nth-child(' + i + ') > .footer-04__list > :nth-child(' + j + ') > .footer-04__link')
+
+        if (i == 3) if (i == 4) {
+          if (j == 1 || j == 2) {
+            domValidation(':nth-child(' + i + ') > .footer-04__list > :nth-child(' + j + ') > .footer-04__link', j + 17)
+          }
+          else
+            validation(j + 17)
+        }
+
+        if (i == 5) {
+          if (j == 1 || j == 4) {
+            domValidation(':nth-child(' + i + ') > .footer-04__list > :nth-child(' + j + ') > .footer-04__link', j + 23)
+          }
+          else
+            validation(j + 23)
+        }
+
+        navigate(0)
+        validation(0)
+
+      }
+    }
+  }
+}
+
+//Function for Validating Share Icons in footer Except
+export function ShareIcons() {
+
+  for (var i = 1; i <= 4; i++) {
+    buttonClick('#\\31 6010-230037 > div.bg-black-color > footer > div > div.container > div > div:nth-child(6) > div > div > ul > li:nth-child(' + i + ') > a')
+    domValidation('#\\31 6010-230037 > div.bg-black-color > footer > div > div.container > div > div:nth-child(6) > div > div > ul > li:nth-child(' + i + ') > a', i + 10)
+    navigate(0)
+    validation(0)
+  }
+}
--- cypress/integration/Smoke-test.spec.js
+++ cypress/integration/Smoke-test.spec.js
@@ -1,33 +1,49 @@
 /// <reference types="cypress' / >
 import '../fixtures/inkforall_url.json'
-import { buttonClick, navbar, navigate, validation, downloadApp, footer } from '../Page_Objects/ExportFunctions';
-import '../Page_Objects/Session';
+import { buttonClick, navbar, navigate, validation, downloadApp, footer, ShareIcons, NavigateForlocalStorage } from '../Page_Objects/ExportFunctions';
+import '../Page_Objects/Local_Storage'
 
-before(()=>{
-    navigate(0)
-    cy.viewport('iphone-6')
+before(() => {
+//NavigateForlocalStorage();
+navigate(0)
+//cy.viewport('iphone-6')
 })
 
-describe('Smoke Test For InkForALl',()=>{
-    it('Verify All Links',() => { 
+describe('Smoke Test For InkForALl', () => {
+    
+    //Validating the Testing.inkforAll Link
+    it('Verify All Links', () => {
         validation(0)
     })
-    it('Verify Header Links',() => { 
-        buttonClick('.nav-02__logo_img')
-        validation(29)
-        navbar()
+    
+    //Verifying the All the Links in Header
+    it('Verify Header Links', () => {
+        //cy.restoreLocalStorage()
+        buttonClick('.nav-02__logo_img')  //Function is exported from ExportFuntions.js File 
+        validation(29)                    //Function is exported from ExportFuntions.js File 
+        navbar()                         //Function is exported from ExportFuntions.js File 
     })
-    it('Verify Download Apps Links',() => { 
 
-        buttonClick('.header-23__cta_box > .buttons-set > .buttons-set__list > .buttons-set__item > .button > .button__text')
-        validation(7)
+    //Verifying the Ink Free Forever
+    it('Verify Use Ink Free Forever Link', () => {
+        buttonClick('.header-23__cta_box > .buttons-set > .buttons-set__list > .buttons-set__item > .button > .button__text')  //Function is exported from ExportFuntions.js File 
+        validation(7)//Function is exported from ExportFuntions.js File 
         cy.go('back')
-        downloadApp('.header-61__cta_box > .buttons-set > .buttons-set__list > :nth-child(','.header-61__text > p > a')
-        downloadApp('[style="position: relative; z-index: 10;"] > .header-61 > .container > .header-61__left > .header-61__left_content > .header-61__left_content_box > .header-61__cta_box > .buttons-set > .buttons-set__list > :nth-child(','#header-61-259941 > div:nth-child(2) > header > div.container.container--large.header-61__container > div > div > div > div.header-61__text.content_box > p > a')
-
-    })
-    it('Verify Footer Links',() => { 
-     footer()
     })
     
+    //Verifying All the Download Apps Links
+    it('Verify Download Apps Links', () => {
+        downloadApp('.header-61__cta_box > .buttons-set > .buttons-set__list > :nth-child(', '.header-61__text > p > a')  //Function is exported from ExportFuntions.js File 
+        downloadApp('[style="position: relative; z-index: 10;"] > .header-61 > .container > .header-61__left > .header-61__left_content > .header-61__left_content_box > .header-61__cta_box > .buttons-set > .buttons-set__list > :nth-child(', '#header-61-259941 > div:nth-child(2) > header > div.container.container--large.header-61__container > div > div > div > div.header-61__text.content_box > p > a') //Function is exported from ExportFuntions.js File 
     })
+
+    //Verifying All the Links in Footer except Share Icons
+    it('Verify Footer Links', () => {
+        footer()     //Function is exported from ExportFuntions.js File 
+    })
+    //Verfiying the Share Icons in footer
+    it('Verify ShareIcons Links', () => {
+        ShareIcons()  //Function is exported from ExportFuntions.js File 
+    })
+
+})
